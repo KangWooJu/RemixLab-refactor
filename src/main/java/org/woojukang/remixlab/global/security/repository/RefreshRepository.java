f197aafba6318f9fccfb5b2b7b8227740a46c650
KangWooJu <space1102@naver.com>
2026-04-07 17:39:23 +0900

    refactor : add refresh token exception on method

--- src/main/java/org/woojukang/remixlab/global/security/repository/RefreshRepository.java
+++ src/main/java/org/woojukang/remixlab/global/security/repository/RefreshRepository.java
@@ -3,6 +3,8 @@ package org.woojukang.remixlab.global.security.repository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
+import org.woojukang.remixlab.global.config.exception.BaseExceptionEnum;
+import org.woojukang.remixlab.global.config.exception.domain.BaseException;
 
 import java.util.concurrent.TimeUnit;
 
@@ -28,14 +30,25 @@ public class RefreshRepository {
 
     // 조회
     public Object findByKey(String key){
-        return redisTemplate
+
+        Object value = redisTemplate
                 .opsForValue()
                 .get(key);
+
+        if(value == null){
+            throw new BaseException(BaseExceptionEnum.REFRESH_TOKEN_NOT_FOUND);
+        }
+
+        return value;
     }
 
     // 삭제
     public void delete(String key){
         redisTemplate
                 .delete(key);
+    }
+
+    public boolean exists(String key){
+        return redisTemplate.hasKey(key);
     }
 }
