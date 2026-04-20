47b47fee64681ed36bd574cb811215de5df1f456
KangWooJu <space1102@naver.com>
2026-04-20 19:20:03 +0900

    refactor : feat: implement latency and throughput monitoring for OpenAI image processing

--- src/main/java/org/woojukang/remixlab/global/metrics/PhotoGenerationMetrics.java
+++ src/main/java/org/woojukang/remixlab/global/metrics/PhotoGenerationMetrics.java
@@ -1,7 +1,9 @@
 package org.woojukang.remixlab.global.metrics;
 
+import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,19 @@ import org.springframework.stereotype.Component;
 public class PhotoGenerationMetrics implements AIMetrics{
 
     private final MeterRegistry meterRegistry;
+    private Counter successCounter;
+    private Counter failCounter;
+
+    @PostConstruct
+    public void init() {
+        this.successCounter = Counter.builder("openai.image.success.count")
+                .description("Number of successful image generations")
+                .register(meterRegistry);
+
+        this.failCounter = Counter.builder("openai.image.fail.count")
+                .description("Number of failed image generations")
+                .register(meterRegistry);
+    }
 
     @Override
     public Timer.Sample start() {
@@ -18,8 +33,18 @@ public class PhotoGenerationMetrics implements AIMetrics{
 
     @Override
     public void stop(Timer.Sample sample) {
-        sample.stop(Timer.builder("openai.image.total")
+        sample.stop(Timer.builder("openai.image.latency")
                 .description("End-to-end latency for OpenAI image generation")
                 .register(meterRegistry));
     }
+
+    public void success() {
+        successCounter.increment();
+    }
+
+    public void fail() {
+        failCounter.increment();
+    }
+
+
 }
